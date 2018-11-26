36b7d98313c6be00420df3416da4b0e54b35b144
Thomas Deniffel <tdeniffel@googlemail.com>
2018-11-26 07:04:27 +0100

    working

--- src/main/java/io/deniffel/tcr/gitConnector/GitConnector.java
+++ src/main/java/io/deniffel/tcr/gitConnector/GitConnector.java
@@ -1,10 +1,17 @@
 package io.deniffel.tcr.gitConnector;
 
 import io.deniffel.tcr.core.Commiter;
+import io.deniffel.tcr.core.Reverter;
 
-public class GitConnector implements Commiter {
+public class GitConnector implements Commiter, Reverter {
+
     @Override
     public void commit() {
+
+    }
+
+    @Override
+    public void revert() {
 
     }
 }
--- src/test/java/gitConnector/GitConnectorTests.java
+++ src/test/java/gitConnector/GitConnectorTests.java
@@ -1,10 +1,13 @@
 package gitConnector;
 
 import io.deniffel.tcr.core.Commiter;
+import io.deniffel.tcr.core.Reverter;
 import io.deniffel.tcr.gitConnector.GitConnector;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class GitConnectorTests {
 
@@ -17,6 +20,11 @@ public class GitConnectorTests {
 
     @Test
     public void instanceOfCommiter() {
-        Assert.assertTrue(gitConnector instanceof Commiter);
+        assertTrue(gitConnector instanceof Commiter);
+    }
+
+    @Test
+    public void instanceOfReverter() {
+        assertTrue(gitConnector instanceof Reverter);
     }
 }
