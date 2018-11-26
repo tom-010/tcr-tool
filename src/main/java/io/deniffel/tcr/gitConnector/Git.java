4a8a450807d530a3f057a5b3d3b5eeeb8b020e22
Thomas Deniffel <tdeniffel@googlemail.com>
2018-11-26 07:58:20 +0100

    working

--- src/main/java/io/deniffel/tcr/gitConnector/Git.java
+++ src/main/java/io/deniffel/tcr/gitConnector/Git.java
@@ -6,36 +6,40 @@ import java.io.InputStreamReader;
 
 public class Git {
 
+    private ProcessRunner process = new ProcessRunner();
 
     public void commit() {
-        execute("git", "commit", "-am", "working");
+        process.execute("git", "commit", "-am", "working");
     }
 
-    private void execute(String... command) {
-        try {
-            doExecute(command);
-        } catch (IOException err) {
-            err.printStackTrace();
-        }
-    }
-
-    private void doExecute(String[] command) throws IOException {
-        Process p = Runtime.getRuntime().exec(command);
-        String result = readProcessOutput(p);
-        System.out.println(result);
-    }
-
-    private String readProcessOutput(Process p) throws IOException {
-        String result = "";
-        try(BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
-            String line;
-
-            while ((line = input.readLine()) != null) {
-                result += line + "\n";
+    public static class ProcessRunner {
+        public void execute(String... command) {
+            try {
+                doExecute(command);
+            } catch (IOException err) {
+                err.printStackTrace();
             }
         }
-        return result;
+
+        private void doExecute(String[] command) throws IOException {
+            Process p = Runtime.getRuntime().exec(command);
+            String result = readProcessOutput(p);
+            System.out.println(result);
+        }
+
+        private String readProcessOutput(Process p) throws IOException {
+            String result = "";
+            try(BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+                String line;
+
+                while ((line = input.readLine()) != null) {
+                    result += line + "\n";
+                }
+            }
+            return result;
+        }
     }
+
 
 
     public static void main(String... args) {
