a2dc8b78769eefe3ed71c1b7459eb9b8a69b340a
Thomas Deniffel <tdeniffel@googlemail.com>
2018-11-26 07:03:47 +0100

    working

---
+++ src/main/java/io/deniffel/tcr/gitConnector/GitConnector.java
@@ -0,0 +1,10 @@
+package io.deniffel.tcr.gitConnector;
+
+import io.deniffel.tcr.core.Commiter;
+
+public class GitConnector implements Commiter {
+    @Override
+    public void commit() {
+
+    }
+}
--- src/test/java/gitConnector/GitConnectorTests.java
+++ src/test/java/gitConnector/GitConnectorTests.java
@@ -1,11 +1,22 @@
 package gitConnector;
 
+import io.deniffel.tcr.core.Commiter;
+import io.deniffel.tcr.gitConnector.GitConnector;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GitConnectorTests {
 
+    GitConnector gitConnector;
+
+    @Before
+    public void setUp() throws Exception {
+        gitConnector = new GitConnector();
+    }
+
     @Test
-    public void nothing() {
-        
+    public void instanceOfCommiter() {
+        Assert.assertTrue(gitConnector instanceof Commiter);
     }
 }
