eb822637d86cdba5d94778dfcb4ddba725ad6b6a
Thomas Deniffel <tdeniffel@googlemail.com>
2018-11-26 07:53:44 +0100

    working

--- src/main/java/io/deniffel/tcr/gitConnector/Git.java
+++ src/main/java/io/deniffel/tcr/gitConnector/Git.java
@@ -1,9 +1,35 @@
 package io.deniffel.tcr.gitConnector;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
 public class Git {
 
-    public void commit() {
 
+    public void commit() {
+        execute("git", "commit", "-am", "working");
+    }
+
+    protected void execute(String... command) {
+        try {
+            Process p = Runtime.getRuntime().exec(command);
+
+            try(BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+                String line;
+
+                while ((line = input.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+    }
+
+    public static void main(String... args) {
+        new Git().commit();
     }
 
     public void reset() {
