8b5aaa2b0f7db959128b6662ca47bc00dd6ac1d2
Thomas Deniffel <tdeniffel@googlemail.com>
2018-11-26 07:58:56 +0100

    working

---
+++ src/main/java/io/deniffel/tcr/gitConnector/ProcessRunner.java
@@ -0,0 +1,33 @@
+package io.deniffel.tcr.gitConnector;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ProcessRunner {
+    public void execute(String... command) {
+        try {
+            doExecute(command);
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+    }
+
+    private void doExecute(String[] command) throws IOException {
+        Process p = Runtime.getRuntime().exec(command);
+        String result = readProcessOutput(p);
+        System.out.println(result);
+    }
+
+    private String readProcessOutput(Process p) throws IOException {
+        String result = "";
+        try(BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+            String line;
+
+            while ((line = input.readLine()) != null) {
+                result += line + "\n";
+            }
+        }
+        return result;
+    }
+}
