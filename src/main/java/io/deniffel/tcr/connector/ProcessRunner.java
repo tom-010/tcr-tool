f3d3cdce86d4bda062b9480e7edcdc8a79552233
Thomas Deniffel <tdeniffel@googlemail.com>
2018-11-27 07:10:27 +0100

    Prototype works

--- src/main/java/io/deniffel/tcr/connector/ProcessRunner.java
+++ src/main/java/io/deniffel/tcr/connector/ProcessRunner.java
@@ -11,17 +11,18 @@ public class ProcessRunner {
     public boolean execute(String... command) {
         try {
             return doExecute(command) == SUCCESSFUL_EXIT;
-        } catch (IOException err) {
+        } catch (IOException | InterruptedException err) {
             err.printStackTrace();
             return false;
         }
     }
 
-    private int doExecute(String[] command) throws IOException {
+    private int doExecute(String[] command) throws IOException, InterruptedException {
         Process p = Runtime.getRuntime().exec(command);
         String result = readProcessOutput(p);
         System.out.println(result);
-        return 0; // p.exitValue();
+        p.waitFor();
+        return p.exitValue();
     }
 
     private String readProcessOutput(Process p) throws IOException {
