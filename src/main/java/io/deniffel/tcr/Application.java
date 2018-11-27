bef37a781e2f31342390884a2468023b0aa68d28
Thomas Deniffel <tdeniffel@googlemail.com>
2018-11-27 06:59:34 +0100

    working

--- src/main/java/io/deniffel/tcr/Application.java
+++ src/main/java/io/deniffel/tcr/Application.java
@@ -1,8 +1,19 @@
 package io.deniffel.tcr;
 
+import io.deniffel.tcr.connector.git.Git;
+import io.deniffel.tcr.connector.git.GitConnector;
+import io.deniffel.tcr.connector.gradle.Gradle;
+import io.deniffel.tcr.connector.gradle.GradleConnector;
+import io.deniffel.tcr.strategies.ClassicTcr;
+
 public class Application {
 
     public static void main(String... args) {
+
+        GradleConnector gradle = new GradleConnector(new Gradle());
+        GitConnector git = new GitConnector(new Git());
+
+        ClassicTcr tcr = new ClassicTcr();
 
     }
 }
