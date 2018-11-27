1417fc6e69bc1eb76726691dca4b8dfd0bd40aef
Thomas Deniffel <tdeniffel@googlemail.com>
2018-11-27 06:49:53 +0100

    working

--- src/test/java/io/deniffel/tcr/connector/gradle/GradleConnectorTests.java
+++ src/test/java/io/deniffel/tcr/connector/gradle/GradleConnectorTests.java
@@ -40,22 +40,4 @@ public class GradleConnectorTests {
         gradleConnector.test();
         assertTrue(gradle.testWasTriggered);
     }
-
-    public static class GradleMock extends Gradle {
-        public boolean buildWasTriggered = false;
-        public boolean testWasTriggered = true;
-
-        @Override
-        public boolean build() {
-            buildWasTriggered = true;
-            return true;
-        }
-
-        @Override
-        public boolean test() {
-            testWasTriggered = true;
-            return true;
-        }
-    }
-
 }
--- src/test/java/io/deniffel/tcr/connector/gradle/GradleMock.java
+++ src/test/java/io/deniffel/tcr/connector/gradle/GradleMock.java
@@ -1,4 +1,18 @@
 package io.deniffel.tcr.connector.gradle;
 
-public class GradleMock {
+public class GradleMock extends Gradle {
+    public boolean buildWasTriggered = false;
+    public boolean testWasTriggered = true;
+
+    @Override
+    public boolean build() {
+        buildWasTriggered = true;
+        return true;
+    }
+
+    @Override
+    public boolean test() {
+        testWasTriggered = true;
+        return true;
+    }
 }
